e307054504520f500c9b722ff099a8df2f59289a
Harry <hareshwarp649>
2022-03-13 21:24:17 +0530

    Order list Abstract data Type

---
+++ Datastruc1/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure17
+{
+    internal class Node<T>
+    {
+
+		public T data;
+		public Node<T> next;
+
+		public Node(T data2)
+		{
+			this.data = (T)data2; 
+			this.next = null; 
+		}
+
+		
+		public T getdata()
+		{
+			return data;
+		}
+
+		
+		Node<T> getNext()
+		{
+			return next;
+		}
+
+		
+		public void setData(T newData)
+		{
+			this.data = newData;
+		}
+
+		public void setNext(Node<T> newNext)
+		{
+			this.next = newNext;
+		}
+
+	}
+}
+    
+
