ea96f4d06a0bdfad577cd8f6ecc16343331f3740
Harry <hareshwarp649>
2022-03-13 22:41:22 +0530

    Balance parantheses Arithmatic Expression program

---
+++ BalanceParanthesis/BalancedParan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruct17b
+{
+    internal class BalancedParan
+    {
+		private int maxSize;
+		private long[] stackArray;
+		private int top;
+
+		 
+		public BalancedParan(int s)
+		{
+			maxSize = s;
+			stackArray = new long[maxSize];
+			top = -1;
+		}
+
+		 
+		public void push(long j)
+		{
+			stackArray[++top] = j;
+		}
+
+		
+		public long pop()
+		{
+			return stackArray[top--];
+		}
+
+		
+		public long peek()
+		{
+			return stackArray[top];
+		}
+
+		  
+		public bool isEmpty()
+		{
+			return (top == -1);
+		}
+
+		   
+		public bool isFull()
+		{
+			return (top == maxSize - 1);
+		}
+	}
+}
---
+++ BalanceParanthesis/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruct17b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Enter max size: ");
+			int n = int.Parse(Console.ReadLine());
+
+
+			
+			BalancedParan stack = new BalancedParan(n);
+			Console.WriteLine();
+			Console.WriteLine("Parenthesis Matching");
+			Console.WriteLine();
+
+
+			
+			Console.WriteLine("Enter expression: ");
+			string exp = "(5+6)∗(7+8)/(4+3)(5+6)";
+
+			Console.WriteLine("Matches and Mismatches: ");
+
+			for (int i = 0; i < exp.Length - 1; i++)
+			{
+				char ch = exp[i];
+
+				if (ch == '(')
+				{
+					stack.push(i);
+				}
+				else if (ch == ')')
+				{
+					try
+					{
+						long p = (stack.pop() + 1);
+						Console.WriteLine("')' at index " + (i + 1) + " matched with '(' at index " + p);
+					}
+					catch (Exception )
+					{
+						Console.WriteLine("')' at index " + (i + 1) + " is unmatched");
+					}
+				}
+				while (!stack.isEmpty())
+				{
+					Console.WriteLine("'(' at index " + (stack.pop() + 1) + " is unmatched");
+				}
+			}
+		}
+    }
+}
