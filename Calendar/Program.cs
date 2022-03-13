33215e586ef4fe83b8dc4725bfc7f13b959e8d3e
Harry <hareshwarp649>
2022-03-13 23:04:58 +0530

    Creat Program to display calendar

---
+++ Calendar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruc17c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendar cal = new Calendar();
+            cal.DayCalendar();
+        }
+    }
+}
